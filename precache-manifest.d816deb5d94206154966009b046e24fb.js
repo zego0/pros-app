44f618cbf930781f00567bed81f2374d6305299b
zego0 <zeynep9698@hotmail.com>
2020-09-11 00:58:47 +0300

    Updates

---
+++ precache-manifest.d816deb5d94206154966009b046e24fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8d5cbc75ea18bcfa44c74e743e408b",
+    "url": "/pros-app/index.html"
+  },
+  {
+    "revision": "e99c3e3d9e5c2ff76f6d",
+    "url": "/pros-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1beea375086782296535",
+    "url": "/pros-app/static/css/main.71ac720c.chunk.css"
+  },
+  {
+    "revision": "e99c3e3d9e5c2ff76f6d",
+    "url": "/pros-app/static/js/2.13036645.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/pros-app/static/js/2.13036645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1beea375086782296535",
+    "url": "/pros-app/static/js/main.cee24de3.chunk.js"
+  },
+  {
+    "revision": "020caf6f6e17c29ee81d",
+    "url": "/pros-app/static/js/runtime-main.5737cdf7.js"
+  }
+]);
